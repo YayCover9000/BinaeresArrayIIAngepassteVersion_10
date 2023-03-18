7887a252a1d20c287db893e90466f52a55d902fd
YayCover9000 <yassuanf@googlemail.com>
2023-03-18 14:21:29 +0100

    Hier wird ein einfaches Integer-Array rows verwendet, um die passenden Zeilen zu speichern. Wir zählen auch die Anzahl der passenden Zeilen in der Variablen count. Am Ende der Schleife wird überprüft, ob es überhaupt passende Zeilen gibt (d.h. count ist größer als Null). Wenn es keine passenden Zeilen gibt, wird eine Fehlermeldung ausgegeben. Andernfalls wird jedes Element des rows-Arrays durchlaufen und jede passende Zeile ausgegeben.

--- src/BinaerArrayII.java
+++ src/BinaerArrayII.java
@@ -7,6 +7,7 @@ public class BinaerArrayII {
             System.out.println("Das übergebene Array ist null.");
             return;
         }
+        int[] rows = new int[matrix.length];
         int count = 0;
         for (int i = 0; i < matrix.length; i++) {
             int zeros = 0;
@@ -16,7 +17,22 @@ public class BinaerArrayII {
                 }
             }
             if (zeros % 2 == 0) {
+                rows[count] = i;
+                count++;
+            }
+        }
+        if (count == 0) {
+            System.out.println("Keine Zeilen mit einer geraden Anzahl von Nullen gefunden.");
+        } else {
+            for (int i = 0; i < count; i++) {
+                System.out.print("Zeile " + rows[i] + ": ");
+                for (int j = 0; j < matrix[rows[i]].length; j++) {
+                    System.out.print(matrix[rows[i]][j] + " ");
+                }
+                System.out.println();
+                /*
                 System.out.print("Zeile " + i + ": ");
+
                 for (int j = 0; j < matrix[i].length; j++) {
                     System.out.print(matrix[i][j] + " ");
                 }
@@ -26,6 +42,9 @@ public class BinaerArrayII {
         }
         if (count == 0) {
             System.out.println("Keine Zeilen mit einer geraden Anzahl von Nullen gefunden.");
+
+          */
+            }
         }
     }
 }
