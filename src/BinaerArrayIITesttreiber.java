c4c77b5a7b834d23e78ab7e82c9294030eaa1369
YayCover9000 <yassuanf@googlemail.com>
2023-03-18 15:19:22 +0100

    shorted

--- src/BinaerArrayIITesttreiber.java
+++ src/BinaerArrayIITesttreiber.java
@@ -5,24 +5,20 @@ public class BinaerArrayIITesttreiber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Bitte geben Sie n ein: ");
-        if (!scanner.hasNextInt()) {
-            System.out.println("Keine gültige Eingabe.");
-            return;
-        }
         int n = scanner.nextInt();
+        if (n == 0) {
+            System.out.println("Das übergebene Array ist null.");
+            return;
 
+        }
         int[][] matrix = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = (int) (Math.random() * 2);
             }
         }
-        if (n == 0) {
-            System.out.println("Das übergebene Array ist null.");
-            return;
-        } else {
-            BinaerArrayII.ausgeben(matrix);
-        }
+        BinaerArrayII.ausgeben(matrix);
+        scanner.close();
     }
 }
 
